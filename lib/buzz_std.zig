0b5b073dd4f08fa531093421e4c7101e033d443c
Benoit Giannangeli <benoit.giannangeli@boursorama.fr>
2021-09-24 23:49:13 +0200

    std.print
    Won't be useful until we have toString

--- lib/buzz_std.zig
+++ lib/buzz_std.zig
@@ -19,6 +19,22 @@ export fn assertTypeDef() *api.ObjTypeDef {
     return type_def;
 }
 
+export fn print(vm: *api.VM) bool {
+    _ = std.io.getStdOut().write(std.mem.span(vm.bz_peek(0).bz_valueToString().?)) catch {
+        vm.bz_throw("Could not print.");
+        return false;
+    };
+
+    return false;
+}
+
+export fn printTypeDef() *api.ObjTypeDef {
+    var type_def: *api.ObjTypeDef = api.ObjTypeDef.bz_newFunctionType("assert", null).?;
+    _ = type_def.bz_addFunctionArgument("value", api.ObjTypeDef.bz_stringType().?);
+
+    return type_def;
+}
+
 export fn openLib() [*:0]const u8 {
-    return "assert";
+    return "assert,print";
 }
