c9acd4c8baca2a77a72834fdd2ac46d5b72b6643
Benoit Giannangeli <benoit.giannangeli@boursorama.fr>
2022-03-24 10:23:53 +0100

    feat(std): std.Error basic error type

--- lib/buzz_std.zig
+++ lib/buzz_std.zig
@@ -1,16 +1,6 @@
 const std = @import("std");
 const api = @import("buzz_api.zig");
 
-export fn assert(vm: *api.VM) bool {
-    var condition: bool = vm.bz_peek(1).bz_valueToBool();
-
-    if (!condition) {
-        vm.bz_throw(vm.bz_peek(0));
-    }
-
-    return false;
-}
-
 export fn print(vm: *api.VM) bool {
     _ = std.io.getStdOut().write(std.mem.sliceTo(vm.bz_peek(0).bz_valueToString().?, 0)) catch {
         // TODO: throw something?
