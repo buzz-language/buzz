ae8152d4413cc06574253dfdd74da78749683fa2
Benoit Giannangeli <benoit.giannangeli@boursorama.fr>
2022-03-24 10:57:36 +0100

    feat(fs): fs.mkDir, fs.rm (#25)

--- lib/buzz_fs.zig
+++ lib/buzz_fs.zig
@@ -2,12 +2,44 @@ const std = @import("std");
 const api = @import("./buzz_api.zig");
 const utils = @import("../src/utils.zig");
 
-export fn mkDir(_: *api.VM) bool {
-    unreachable;
+export fn mkDir(vm: *api.VM) bool {
+    const filename: []const u8 = std.mem.sliceTo(api.Value.bz_valueToString(vm.bz_peek(0)) orelse "", 0);
+
+    if (std.fs.path.isAbsolute(filename)) {
+        std.fs.makeDirAbsolute(filename) catch {
+            vm.bz_throwString("Could not create directory");
+
+            return false;
+        };
+    } else {
+        std.fs.cwd().makeDir(filename) catch {
+            vm.bz_throwString("Could not create directory");
+
+            return false;
+        };
+    }
+
+    return false;
 }
 
-export fn rm(_: *api.VM) bool {
-    unreachable;
+export fn rm(vm: *api.VM) bool {
+    const filename: []const u8 = std.mem.sliceTo(api.Value.bz_valueToString(vm.bz_peek(0)) orelse "", 0);
+
+    if (std.fs.path.isAbsolute(filename)) {
+        std.fs.deleteTreeAbsolute(filename) catch {
+            vm.bz_throwString("Could not delete file or directory");
+
+            return false;
+        };
+    } else {
+        std.fs.cwd().deleteTree(filename) catch {
+            vm.bz_throwString("Could not delete file or directory");
+
+            return false;
+        };
+    }
+
+    return false;
 }
 
 export fn move(_: *api.VM) bool {
