592410223a7bfcf87f5d66a0ad9d45d71c4f8053
Benoit Giannangeli <benoit.giannangeli@boursorama.fr>
2022-07-16 01:09:07 +0200

    build.zig: a little cleaner

--- build.zig
+++ build.zig
@@ -17,56 +17,35 @@ pub fn build(b: *Builder) void {
     lib.setMainPkgPath(".");
     lib.setBuildMode(build_mode);
 
-    var std_lib = b.addSharedLibrary("std", "lib/buzz_std.zig", .{ .unversioned = {} });
-    std_lib.setTarget(target);
-    std_lib.install();
-    std_lib.setMainPkgPath(".");
-    std_lib.setBuildMode(build_mode);
-    std_lib.linkLibrary(lib);
-
-    var io_lib = b.addSharedLibrary("io", "lib/buzz_io.zig", .{ .unversioned = {} });
-    io_lib.setTarget(target);
-    io_lib.install();
-    io_lib.setMainPkgPath(".");
-    io_lib.setBuildMode(build_mode);
-    io_lib.linkLibrary(lib);
-
-    var gc_lib = b.addSharedLibrary("gc", "lib/buzz_gc.zig", .{ .unversioned = {} });
-    gc_lib.setTarget(target);
-    gc_lib.install();
-    gc_lib.setMainPkgPath(".");
-    gc_lib.setBuildMode(build_mode);
-    gc_lib.linkLibrary(lib);
-
-    var os_lib = b.addSharedLibrary("os", "lib/buzz_os.zig", .{ .unversioned = {} });
-    os_lib.setTarget(target);
-    os_lib.install();
-    os_lib.setMainPkgPath(".");
-    os_lib.setBuildMode(build_mode);
-    os_lib.linkLibrary(lib);
-
-    var fs_lib = b.addSharedLibrary("fs", "lib/buzz_fs.zig", .{ .unversioned = {} });
-    fs_lib.setTarget(target);
-    fs_lib.install();
-    fs_lib.setMainPkgPath(".");
-    fs_lib.setBuildMode(build_mode);
-    fs_lib.linkLibrary(lib);
-
-    var math_lib = b.addSharedLibrary("math", "lib/buzz_math.zig", .{ .unversioned = {} });
-    math_lib.setTarget(target);
-    math_lib.install();
-    math_lib.setMainPkgPath(".");
-    math_lib.setBuildMode(build_mode);
-    math_lib.linkLibrary(lib);
-
     b.default_step.dependOn(&exe.step);
     b.default_step.dependOn(&lib.step);
-    b.default_step.dependOn(&std_lib.step);
-    b.default_step.dependOn(&io_lib.step);
-    b.default_step.dependOn(&gc_lib.step);
-    b.default_step.dependOn(&os_lib.step);
-    b.default_step.dependOn(&fs_lib.step);
-    b.default_step.dependOn(&math_lib.step);
+
+    const lib_paths = [_][]const u8{
+        "lib/buzz_std.zig",
+        "lib/buzz_io.zig",
+        "lib/buzz_gc.zig",
+        "lib/buzz_os.zig",
+        "lib/buzz_fs.zig",
+        "lib/buzz_math.zig",
+    };
+    const lib_names = [_][]const u8{
+        "std",
+        "io",
+        "gc",
+        "os",
+        "fs",
+        "math",
+    };
+
+    for (lib_paths) |lib_path, index| {
+        var std_lib = b.addSharedLibrary(lib_names[index], lib_path, .{ .unversioned = {} });
+        std_lib.setTarget(target);
+        std_lib.install();
+        std_lib.setMainPkgPath(".");
+        std_lib.setBuildMode(build_mode);
+        std_lib.linkLibrary(lib);
+        b.default_step.dependOn(&std_lib.step);
+    }
 
     const play = b.step("run", "Run buzz");
     const run = exe.run();
