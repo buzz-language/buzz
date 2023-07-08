f360419807e32b009dc202afdda75c7a1750fc7d
Locria Cyber <74560659+iacore@users.noreply.github.com>
2023-07-08 12:06:28 +0000

    fix: linking on Linux

--- src/jmp.zig
+++ src/jmp.zig
@@ -1,3 +1,8 @@
-pub const jmp = @cImport({
+// FIXME: remove setjmp/longjmp  https://github.com/ziglang/zig/issues/1656#issuecomment-840190292
+// tip: use minicoro instead  https://github.com/edubart/minicoro
+
+const c_jmp = @cImport({
     @cInclude("setjmp.h");
 });
+
+pub const jmp = c_jmp;
