73bb05010d68dc71c139cccabfc6bd6860794c8f
David <87927264+Rexicon226@users.noreply.github.com>
2023-10-31 13:05:52 -0700

    nit: fix typo

--- src/reporter.zig
+++ src/reporter.zig
@@ -8,7 +8,7 @@ const Scanner = @import("scanner.zig").Scanner;
 
 const Self = @This();
 
-// Do not reorder whitout updating documentation, values are explicit so they can be retrieved easily
+// Do not reorder without updating documentation, values are explicit so they can be retrieved easily
 pub const Error = enum(u8) {
     already_conforming_protocol = 0,
     arguments_count = 1,
