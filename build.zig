894bdfa8ef17d5a1790bf5654e455949646a604d
Benoit Giannangeli <giann008@gmail.com>
2024-01-15 22:25:10 +0100

    zig 0.12.0-dev.2208+4debd4338

--- build.zig
+++ build.zig
@@ -88,7 +88,7 @@ fn get_buzz_prefix(b: *Build) []const u8 {
 pub fn build(b: *Build) !void {
     // Check minimum zig version
     const current_zig = builtin.zig_version;
-    const min_zig = std.SemanticVersion.parse("0.12.0-dev.2150+63de8a598") catch return;
+    const min_zig = std.SemanticVersion.parse("0.12.0-dev.2208+4debd4338") catch return;
     if (current_zig.order(min_zig).compare(.lt)) {
         @panic(b.fmt("Your Zig version v{} does not meet the minimum build requirement of v{}", .{ current_zig, min_zig }));
     }
