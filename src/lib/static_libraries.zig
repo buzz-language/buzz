9e26bf1e4271bc50c5899219ae1190b79b98f00d
ZenDrx <ynwghosted@icloud.com>
2026-07-24 09:21:30 +0100

    Fix header reference for time in static libraries

--- src/lib/static_libraries.zig
+++ src/lib/static_libraries.zig
@@ -30,7 +30,7 @@ pub const all = [_]Library{
     .{ .header = static_headers.os, .zig_path = "buzz_os.zig", .wasm_native = false },
     .{ .header = static_headers.serialize, .zig_path = "buzz_serialize.zig", .wasm_native = true },
     .{ .header = static_headers.std, .zig_path = "buzz_std.zig", .wasm_native = true },
-    .{ .header = static_header.time, .zig_path = "buzz_time.zig", .wasm_native = false },
+    .{ .header = static_headers.time, .zig_path = "buzz_time.zig", .wasm_native = false },
     .{ .header = static_headers.testing, .zig_path = null, .wasm_native = false },
 };
 
